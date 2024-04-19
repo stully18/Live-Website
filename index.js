e4869c100e3d160931007e5361a17276170d8637
stully18 <stullyrl@gmail.com>
2024-04-19 12:47:57 -0400

    misc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let username;
 
-document.getElementById('submitButton').onclick = function(){
-    username = document.getElementById('textbox').value;
-    document.getElementById('usersName').textContent = 'Hello ' + username+' how are you today?';
-}
+//document.getElementById('submitButton').onclick = function(){
+   // username = document.getElementById('textbox').value;
+    //document.getElementById('usersName').textContent = 'Hello ' + username+' how are you today?';
+//}
+
